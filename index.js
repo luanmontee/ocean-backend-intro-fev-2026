9b9773d58995f3103d5f4caa99c149df8c17ff1a
Luan Monte <luanmontep@gmail.com>
2026-02-10 00:07:48 +0000

    Adicionar rotas para manipulação de personagens e arquivos de requisição correspondentes

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ app.get('/personagens', (req, res) => {
   res.send(lista)
 })
 
+app.get('/personagens/:id', (req, res) => {
+  const id = req.params.id
+  
+  res.send (lista[id])
+})
+
+app.use(express.json())
+
+app.post('/personagens', (req, res) => {
+  const novoPersonagem = req.body.nome
+
+  lista.push(novoPersonagem)
+  res.send("Novo personagem adicionado: " + novoPersonagem)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
